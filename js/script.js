dc388567f1fc40b9dbebc49d54b4d14f16b16d70
Aleksandr Loshkarov <a.loshkarev89@gmail.com>
2017-09-02 21:11:08 +0300

    fixed bugs

--- js/script.js
+++ js/script.js
@@ -11,54 +11,20 @@ function toggler() {
 
 toggler();
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
 // ThumbnailGridExpandingPreview
 
 $(function() {
   Grid.init();
 });
 
-// circle-portfolio
-function circlePortfolioInit(){
-  $('.circle-portfolio').circleProgress({
-  });
-
-  $('.circle-clients').circleProgress({}).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(Math.round(1600 * progress));
-  });
-  $('.circle-project').circleProgress({}).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(Math.round(3200 * progress));
-  });
-  $('.circle-avards').circleProgress({}).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(Math.round(40 * progress));
-  });
-  $('.circle-coffee').circleProgress({}).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(Math.round(20000 * progress));
-  });
-};
-
-$(window).on('scroll', function() {
-  if(isScrolledIntoView('.circle-portfolio')){
-    circlePortfolioInit();
-    $(window).off('scroll');
-  }
+jQuery(document).ready(function( $ ) {
+		$('.counter').counterUp({
+				delay: 10,
+				time: 1000
+		});
 });
 
-function isScrolledIntoView(elem)
-{
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
-
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-};
-
 //circle
-
 function circleInit(){
   $('.circle').circleProgress({
     size: 142,
